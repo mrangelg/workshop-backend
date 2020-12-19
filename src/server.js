7f4ec5de657aaf73161ed332d672efddd03a6f51
Madelin Rangel Grisales <mrangel@utp.edu.co>
2020-12-19 11:28:02 -0500

    Cors

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express, { json } from "express";
+var cors = require("cors");
 // Routes
 import IndexRoutes from "./routes/index.routes";
 import TaskRoutes from "./routes/tasks.routes";
@@ -11,6 +12,7 @@ app.set("port", process.env.PORT || 4000);
 
 // Middleware
 app.use(json());
+app.use(cors());
 
 // Routes
 app.use(IndexRoutes);
